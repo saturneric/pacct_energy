27997af3cf214c1d11d1cc5cf8c2c8f66aeb32db
uosfz <uosfz@student.kit.edu>
2026-03-27 23:37:12 +0100

    errors.cc and documentation

---
+++ errors.c
@@ -0,0 +1,24 @@
+#include "errors.h"
+
+struct pacct_errors pacct_errors = { 0 };
+
+#define PACCT_ERROR_TRANSFORM(name)                                      \
+	pr_info("  " #name " = %llu/%llu\n", pacct_errors.name.positive, \
+		pacct_errors.name.positive + pacct_errors.name.negative);
+void pacct_error_report(void)
+{
+	pr_info("pacct_errors {\n");
+	PACCT_ERRORS
+	pr_info("}\n");
+}
+#undef PACCT_ERROR_TRANSFORM
+
+int pacct_error_trace(struct pacct_error_trace *tr, int cond)
+{
+	if (cond) {
+		tr->positive++;
+	} else {
+		tr->negative++;
+	}
+	return cond;
+}
--- errors.h
+++ errors.h
@@ -1,3 +1,21 @@
+/*
+ * Simple condition tracking functionality.
+ * If you want to track a condition, add a member for the `PACCT_ERRORS` below,
+ * then use `PACCT_ERROR_TRACE` wherever you evaluate the condition.
+ *
+ * Example for tracing `answer == 42`:
+ * ```c
+ * // add this to PACCT_ERRORS below
+ * PACCT_ERROR_TRANSFORM(found_the_answer)
+ *
+ * // add this in your actual code
+ * if (PACCT_ERROR_TRACE(found_the_answer, answer == 42) {
+ * 	// success, answer is 42
+ * } else {
+ * 	// fail
+ * }
+ * ```
+ */
 #ifndef PACCT_ERRORS_H
 #define PACCT_ERRORS_H
 
@@ -18,16 +36,15 @@ int pacct_error_trace(struct pacct_error_trace *tr, int cond);
 	PACCT_ERROR_TRANSFORM(model_negative_energy)                 \
 	PACCT_ERROR_TRANSFORM(model_zero_energy)                     \
 	PACCT_ERROR_TRANSFORM(sched_switch_prev_no_traced_task)      \
-	PACCT_ERROR_TRANSFORM(sched_switch_prev_not_ready)           \
 	PACCT_ERROR_TRANSFORM(sched_switch_prev_not_running)         \
+	PACCT_ERROR_TRANSFORM(sched_switch_prev_wrong_cpu)           \
 	PACCT_ERROR_TRANSFORM(sched_switch_prev_counter_got_smaller) \
-	PACCT_ERROR_TRANSFORM(sched_switch_prev_null_event)          \
 	PACCT_ERROR_TRANSFORM(sched_switch_next_no_traced_task)      \
-	PACCT_ERROR_TRANSFORM(sched_switch_next_not_ready)           \
 	PACCT_ERROR_TRANSFORM(sched_switch_next_already_running)     \
-	PACCT_ERROR_TRANSFORM(sched_switch_next_null_event)          \
 	PACCT_ERROR_TRANSFORM(read_event_count_run_ena_mismatch)     \
-	PACCT_ERROR_TRANSFORM(read_event_count_run_ena_mismatch_big)
+	PACCT_ERROR_TRANSFORM(read_event_count_run_ena_mismatch_big) \
+	PACCT_ERROR_TRANSFORM(counter_bad_read)                      \
+	PACCT_ERROR_TRANSFORM(counter_multiplexed)
 
 #define PACCT_ERROR_TRANSFORM(name) struct pacct_error_trace name;
 struct pacct_errors {
@@ -36,6 +53,9 @@ struct pacct_errors {
 #undef PACCT_ERROR_TRANSFORM
 extern struct pacct_errors pacct_errors;
 
+/*
+ * Reports all errors using `pr_info`.
+ */
 void pacct_error_report(void);
 
 #endif
